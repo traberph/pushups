8a677ad3b7848a0597469cb9dffb16b2b1c33777
Jonathan Rentschler <jonathan.rentschler@uni-konstanz.de>
2025-05-01 22:21:23 +0200

    fix oicdCOnfig

--- frontend/src/services/authConfig.ts
+++ frontend/src/services/authConfig.ts
@@ -1,11 +1,20 @@
 import { AuthProviderProps } from "react-oidc-context";
 
 const oidcConfig: AuthProviderProps = {
-  authority: "http://localhost:3000/auth-proxy/application/o/authorize", // proxied OIDC URL
-  client_id: "RS52eMwMS4xsySHJSVCQ1LCZZR8UAqVcttglr1LI",
-  redirect_uri: window.location.origin + "/callback",
-  response_type: "code",
-  scope: "openid profile email",
+    authority:
+    "https://auth.traberph.de/application/o/authorize", // Your OIDC provider URL
+    client_id: "RS52eMwMS4xsySHJSVCQ1LCZZR8UAqVcttglr1LI", // Your client ID
+    redirect_uri: `http://localhost:3000/callback2`, // Callback URL after authentication
+    post_logout_redirect_uri: `http://localhost:3000/`, // URL to redirect to after logout
+    response_type: 'code',
+    scope: 'openid profile email', // Scopes you need
+    metadata: {
+      authorization_endpoint: "https://auth.traberph.de/application/o/authorize/",
+      token_endpoint: "https://auth.traberph.de/application/o/token/",
+      userinfo_endpoint: "https://auth.traberph.de/application/o/userinfo/",
+      end_session_endpoint: "https://auth.traberph.de/application/o/pushups/end-session/",
+      issuer: "https://auth.traberph.de/application/o/pushups/",
+    },
 };
 
 export default oidcConfig;
--- frontend/src/services/authService.ts
+++ frontend/src/services/authService.ts
@@ -5,10 +5,18 @@ const oidcConfig: UserManagerSettings = {
   authority:
   "https://auth.traberph.de/application/o/authorize", // Your OIDC provider URL
   client_id: "RS52eMwMS4xsySHJSVCQ1LCZZR8UAqVcttglr1LI", // Your client ID
-  redirect_uri: `${window.location.origin}/callback`, // Callback URL after authentication
+  redirect_uri: `${window.location.origin}/callback2`, // Callback URL after authentication
   post_logout_redirect_uri: `${window.location.origin}/`, // URL to redirect to after logout
   response_type: 'code',
   scope: 'openid profile email', // Scopes you need
+  metadata: {
+    authorization_endpoint: "https://auth.traberph.de/application/o/authorize/",
+    token_endpoint: "https://auth.traberph.de/application/o/token/",
+    userinfo_endpoint: "https://auth.traberph.de/application/o/userinfo/",
+    end_session_endpoint: "https://auth.traberph.de/application/o/pushups/end-session/",
+    issuer: "https://auth.traberph.de/application/o/pushups/",
+  },
+
 };
 
 class AuthService {
