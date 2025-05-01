0f996fa464c320c7e5bf45ae1a08b1928a2ab4a6
Jonathan Rentschler <jonathan.rentschler@uni-konstanz.de>
2025-05-01 18:24:28 +0200

    Integrate Tailwind CSS

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite'
+
 
 export default defineConfig({
  base: "/",
- plugins: [react()],
+ plugins: [react(), tailwindcss()],
  preview: {
   port: 3000,
   strictPort: true,
